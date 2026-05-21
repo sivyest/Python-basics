0e3d9957b5c674f39240941611ad0bf3baef8a07
Shreyansh Raj <shreyansh86224@gmail.com>
2026-05-21 10:04:46 +0530

    Add files via upload
    Basic Python programs

---
+++ 01_SETUP/main.py
@@ -0,0 +1,1 @@
+print("Sunny innit")
---
+++ 02_python_syntax/main.py
@@ -0,0 +1,6 @@
+print("sunny innit!")
+
+# learning comment
+#  start anything with hash tag and it becomes comment
+
+# to do that just select the whole code and press ctrl + / 

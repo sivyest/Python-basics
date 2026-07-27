dd9cd754001e478250941ef1e1b74f353ba1bd05
Shreyansh Raj <shreyansh86224@gmail.com>
2026-07-27 20:22:00 +0530

    Update 03_logical_operators.py

--- 07_Operators/03_logical_operators.py
+++ 07_Operators/03_logical_operators.py
@@ -16,3 +16,19 @@ print(False or False)
 print("Not Operator")
 print(not(True) ) 
 print(not(False) ) 
+
+# Python Logical Operators
+
+# and Operator
+x = 10
+y = 5
+
+print("x > 5 and y < 10 -->", x > 5 and y < 10)
+
+
+# or Operator
+print("x < 5 or y < 10 -->", x < 5 or y < 10)
+
+
+# not Operator
+print("not(x > 5) -->", not (x > 5))

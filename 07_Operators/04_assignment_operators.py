5befd78b763df89cadb40535ed8c275cc096db7e
Shreyansh Raj <shreyansh86224@gmail.com>
2026-07-27 20:20:14 +0530

    Update 04_assignment_operators.py

--- 07_Operators/04_assignment_operators.py
+++ 07_Operators/04_assignment_operators.py
@@ -2,3 +2,74 @@ a = 32
 print(a)
 a*=3
 print(a)
+
+# Python Assignment Operators
+
+# = (Assignment Operator)
+a = 10
+print("a =", a)
+
+# += (Add and Assign)
+a += 5
+print("a += 5 -->", a)
+
+# -= (Subtract and Assign)
+a -= 3
+print("a -= 3 -->", a)
+
+# *= (Multiply and Assign)
+a *= 2
+print("a *= 2 -->", a)
+
+# /= (Divide and Assign)
+a /= 4
+print("a /= 4 -->", a)
+
+# //= (Floor Divide and Assign)
+a //= 2
+print("a //= 2 -->", a)
+
+# %= (Modulus and Assign)
+a %= 2
+print("a %= 2 -->", a)
+
+# **= (Exponent and Assign)
+a **= 3
+print("a **= 3 -->", a)
+
+
+# Bitwise Assignment Operators
+
+b = 6
+
+# &= (Bitwise AND and Assign)
+b &= 3
+print("b &= 3 -->", b)
+
+# Reset value of b
+b = 6
+
+# |= (Bitwise OR and Assign)
+b |= 3
+print("b |= 3 -->", b)
+
+# Reset value of b
+b = 6
+
+# ^= (Bitwise XOR and Assign)
+b ^= 3
+print("b ^= 3 -->", b)
+
+# Reset value of b
+b = 6
+
+# <<= (Left Shift and Assign)
+b <<= 1
+print("b <<= 1 -->", b)
+
+# Reset value of b
+b = 6
+
+# >>= (Right Shift and Assign)
+b >>= 1
+print("b >>= 1 -->", b)
